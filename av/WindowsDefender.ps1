e4d031d84872dbf97a018040977ba3553594c29f
Bill Zissimopoulos <billziss@navimatics.com>
2020-02-20 21:18:36 +0200

    av: Windows Defender: install location from registry

--- av/WindowsDefender.ps1
+++ av/WindowsDefender.ps1
@@ -7,7 +7,13 @@ function AvScan-WindowsDefender {
         $ScanPath
     )
 
-    $ScanOut = & 'C:\Program Files\Windows Defender\MpCmdRun.exe' -Scan -ScanType 3 -File $ScanPath -DisableRemediation
+    $AvRoot = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Microsoft\Windows Defender' -Name InstallLocation
+    $AvProg = Join-Path $AvRoot 'MpCmdRun.exe'
+    if (-not (Test-Path $AvProg)) {
+        $AvProg = 'C:\Program Files\Windows Defender\MpCmdRun.exe'
+    }
+
+    $ScanOut = & $AvProg -Scan -ScanType 3 -File $ScanPath -DisableRemediation
     if ($LASTEXITCODE -ne 0) {
         Write-ScanOutput "SCAN: MpCmdRun.exe -Scan -ScanType 3 -File `"$(Split-Path $ScanPath -Leaf)`" -DisableRemediation`n"
         Write-ScanOutput $ScanOut
