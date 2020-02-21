828b6b7a4cfe9afce298bf7306015954024fa105
Bill Zissimopoulos <billziss@navimatics.com>
2020-02-21 21:16:09 +0200

    action.js: async

--- action.js
+++ action.js
@@ -8,15 +8,20 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
-try
+async function run()
 {
-    const files = core.getInput('files', {required: true});
-    const args = ["scan"].concat(files.split(/\r?\n/));
-    console.log(`files = ${files}`)
-    console.log(`args = ${args}`)
-    await exec.exec('./avm.ps1', args);
+    try
+    {
+        const files = core.getInput('files', {required: true});
+        const args = ["scan"].concat(files.split(/\r?\n/));
+        console.log(`files = ${files}`)
+        console.log(`args = ${args}`)
+        await exec.exec('./avm.ps1', args);
+    }
+    catch (error)
+    {
+        core.setFailed(error.message);
+    }
 }
-catch (error)
-{
-    core.setFailed(error.message);
-}
+
+run()
