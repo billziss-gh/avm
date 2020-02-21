4eea46e8ccd328733d2975a51746c4879a5a8dd2
Bill Zissimopoulos <billziss@navimatics.com>
2020-02-21 20:56:19 +0200

    make into github action

---
+++ action.js
@@ -0,0 +1,22 @@
+/*
+ * action.js
+ * avm github action
+ *
+ * Copyright 2020 Bill Zissimopoulos
+ */
+
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+try
+{
+    const files = core.getInput('files', {required: true});
+    const args = ["scan"].concat(files.split(/\r?\n/));
+    console.log(`files = ${files}`)
+    console.log(`args = ${args}`)
+    await exec.exec('./avm.ps1', args);
+}
+catch (error)
+{
+    core.setFailed(error.message);
+}
